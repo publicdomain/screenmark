568f2b511d1610f9b1b3e78513c7ff04108e5b5b
Victor <publicdomainvic@gmail.com>
2021-04-30 08:49:32 -0400

    Update about message

--- ScreenMark/ScreenMark/MainForm.cs
+++ ScreenMark/ScreenMark/MainForm.cs
@@ -558,7 +558,7 @@ namespace ScreenMark
             var aboutForm = new AboutForm(
                 $"About {programTitle}",
                 $"{programTitle} {version.Major}.{version.Minor}.{version.Build}",
-                $"Made for: Curt{Environment.NewLine}DonationCoder.com{Environment.NewLine}Day #119, Week #17 @ April 29, 2021",
+                $"Made for: Curt{Environment.NewLine}DonationCoder.com{Environment.NewLine}Day #120, Week #17 @ April 30, 2021",
                 licenseText,
                 this.Icon.ToBitmap())
             {
