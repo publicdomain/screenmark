7fb8394a53a338259aa6f8c2c1347904b9e393c0
Victor <publicdomainvic@gmail.com>
2021-05-10 19:22:41 -0400

    Update version number
    Update about message

--- ScreenMark/ScreenMark/MainForm.cs
+++ ScreenMark/ScreenMark/MainForm.cs
@@ -658,7 +658,7 @@ namespace ScreenMark
                 $"https://pixabay.com/illustrations/d-glamour-gold-lights-2790573/{Environment.NewLine}{Environment.NewLine}";
 
             // Prepend sponsors
-            licenseText = $"RELEASE SPONSORS:{Environment.NewLine}{Environment.NewLine}* Jesse Reichler{Environment.NewLine}{Environment.NewLine}=========={Environment.NewLine}{Environment.NewLine}" + licenseText;
+            licenseText = $"RELEASE SPONSORS:{Environment.NewLine}{Environment.NewLine}* Jesse Reichler{Environment.NewLine}* Curt{Environment.NewLine}{Environment.NewLine}=========={Environment.NewLine}{Environment.NewLine}" + licenseText;
 
             // Set title
             string programTitle = typeof(MainForm).GetTypeInfo().Assembly.GetCustomAttribute<AssemblyTitleAttribute>().Title;
